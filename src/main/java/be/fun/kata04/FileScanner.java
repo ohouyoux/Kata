13588de6ba919c3dac0964914991b16403a2cc46
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 19:09:49 +0200

    Add java documentation

--- src/main/java/be/fun/kata04/FileScanner.java
+++ src/main/java/be/fun/kata04/FileScanner.java
@@ -25,18 +25,34 @@ public abstract class FileScanner<T> implements DataScanner<T, IOException> {
         this.file = checkNotNull(file, "DataType file should not be null");
     }
 
+
     public T scan() throws IOException {
         // Template Method - http://www.oodesign.com/template-method-pattern.html
         List<String> lines = load(file);
         lines = clean(lines);
 
+        // TODO add a step, the split (or scan) should return a list of type that should then be sorted
+
         return split(lines);
     }
 
+    /**
+     * Loads the data to be scanned from the given {@code file}.
+     *
+     * @param file the {@code File} which contains the data to scan
+     * @return the data to scan as a {@code List} of text lines
+     * @throws IOException if {@code file} could not be read
+     */
     protected List<String> load(final File file) throws IOException {
         return Files.readLines(file, Charsets.UTF_8);
     }
 
+    /**
+     * Removes data lines that can't be scanned.
+     *
+     * @param lines all text lines from the data {@code file}
+     * @return the text lines that can be scanned
+     */
     protected List<String> clean(final List<String> lines) {
         return Lists.newArrayList(Iterables.filter(lines, new Predicate<String>() {
 
@@ -46,5 +62,11 @@ public abstract class FileScanner<T> implements DataScanner<T, IOException> {
         }));
     }
 
+    /**
+     * Splits the given lines into some meaningful format.
+     *
+     * @param lines the raw text line that are scanned and split
+     * @return a meaningful view of the scanned data
+     */
     protected abstract T split(List<String> lines);
 }

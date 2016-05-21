6614a56b6e355898ade5da03751a905899ba14a9
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 22:22:42 +0200

    Add java documentation

--- src/main/java/be/fun/kata04/Weather.java
+++ src/main/java/be/fun/kata04/Weather.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- *
+ * A daily weather sample.
  *
  * @author Olivier Houyoux
  */
@@ -24,6 +24,14 @@ public class Weather implements Serializable {
 
     private int spread;
 
+    /**
+     * Instantiates a new {@code Weather}.
+     *
+     * @param city the name of the city where the weather sample was captured
+     * @param month the month of the sample
+     * @param day the day of the month of the sample
+     * @param spread the temperature spread (difference between the maximum and minimum temp during {@code day})
+     */
     public Weather(final String city, final String month, final int day, final int spread) {
         this.city = checkNotNull(city, "City should not be null");
         this.month = checkNotNull(month, "Month should not be null");
@@ -31,18 +39,40 @@ public class Weather implements Serializable {
         this.spread = spread;
     }
 
+    /**
+     * Returns the name of the city where this {@code Weather} sample was recorded.
+     *
+     * @return the name of the city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Returns the month when this sample was recorded.
+     *
+     * @return the month of this {@code Weather} sample
+     */
     public String getMonth() {
         return month;
     }
 
+    /**
+     * Returns the day of the month when this sample was recorded.
+     *
+     * @return the day of the month of this {@code Weather} sample
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     * Returns the difference between the maximum and minimum temperatures measured on {@code day}.
+     *
+     * @return the temperature spread
+     * @see #getDay()
+     * @see #getMonth()
+     */
     public int getSpread() {
         return spread;
     }

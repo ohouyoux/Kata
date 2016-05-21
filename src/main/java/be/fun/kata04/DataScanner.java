14c10c4cddeedca66dd4fd473694cedd6eed69ef
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 07:40:02 +0200

    Documentation added

--- src/main/java/be/fun/kata04/DataScanner.java
+++ src/main/java/be/fun/kata04/DataScanner.java
@@ -2,8 +2,20 @@ package be.fun.kata04;
 
 import java.io.IOException;
 
-// Strategy
+/**
+ * Defines how to apply scanning.
+ *
+ * @param <T> the type of data that this {@code DataScanner} will produce
+ */
 public interface DataScanner<T> {
 
+    // Strategy
+
+    /**
+     * Scans raw data to produce meaningful information.
+     *
+     * @return the result of the scan
+     * @throws IOException if the raw data could not be read
+     */
     T scan() throws IOException;
 }

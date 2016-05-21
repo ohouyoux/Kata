8a2644fac4d17183d1c6038badc1f905291c0660
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 22:14:32 +0200

    Add java documentation

--- src/main/java/be/fun/kata04/Team.java
+++ src/main/java/be/fun/kata04/Team.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- *
+ * A football team.
  *
  * @author Olivier Houyoux
  */
@@ -21,15 +21,32 @@ public class Team implements Comparable<Team>, Serializable {
 
     private int difference;
 
+    /**
+     * Instantiates a new {@code Team}.
+     *
+     * @param name the name of the {@code Team}
+     * @param score the number of goals that have been scored
+     * @param conceded the number of goals that have been conceded
+     */
     public Team(final String name, final int score, final int conceded) {
         this.name = checkNotNull(name, "Team name should not be null");
         this.difference = Math.abs(score - conceded);
     }
 
+    /**
+     * Returns the name of this {@code Team}.
+     *
+     * @return the name of the {@code Team}
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the difference between the number of goals scored and conceded.
+     *
+     * @return the absolute value of the goal difference
+     */
     public int getDifference() {
         return difference;
     }
@@ -54,6 +71,7 @@ public class Team implements Comparable<Team>, Serializable {
         return false;
     }
 
+    @Override
     public int compareTo(final Team team) {
         return ComparisonChain.start().compare(difference, team.difference).result();
     }

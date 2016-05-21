915b259f2901f37f92812881b24f0f0a7723da0d
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 19:04:42 +0200

    Add java documentation

--- src/main/java/be/fun/kata04/DataType.java
+++ src/main/java/be/fun/kata04/DataType.java
@@ -1,8 +1,17 @@
 package be.fun.kata04;
 
+/**
+ * The different types of data that could be scanned.
+ */
 public enum DataType {
 
+    /**
+     * The weather data type.
+     */
     WEATHER,
 
+    /**
+     * The football data type.
+     */
     FOOTBALL
 }

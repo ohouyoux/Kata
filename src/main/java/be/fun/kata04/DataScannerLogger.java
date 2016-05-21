1c1c63876c41ea7ad24998bce030f5c5c3c7f42d
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 19:03:56 +0200

    Add java documentation

--- src/main/java/be/fun/kata04/DataScannerLogger.java
+++ src/main/java/be/fun/kata04/DataScannerLogger.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- *
+ * Decorates a {@code DataScanner} to log the result of the scan.
  *
  * @param <T> the type of data that this {@code DataScanner} will produce
  * @param <E> the type of {@code Exception} that this {@code DataScanner} may throw while parsing data
@@ -19,14 +19,21 @@ public class DataScannerLogger<T, E extends Exception> implements DataScanner<T,
     private final DataScanner<T, E> scanner;
 
     /**
+     * Instantiates a new {@code DataScannerLogger}.
      *
-     *
-     * @param scanner
+     * @param scanner the scanner whose output must be logged
      */
     public DataScannerLogger(final DataScanner<T, E> scanner) {
         this.scanner = checkNotNull(scanner);
     }
 
+    /**
+     * Scans the data from {@code scanner} and logs its output.
+     *
+     * @return the result of the scan from {@code scanner}
+     * @throws E if the scan failed
+     * @see #scanner
+     */
     public T scan() throws E {
         T data = scanner.scan();
         LOGGER.info(data.toString());

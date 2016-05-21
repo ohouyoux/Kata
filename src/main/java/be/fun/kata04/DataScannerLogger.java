669ee8a17d9c39ddc406481f25c39266a9528689
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-05-21 19:00:53 +0200

    Use an appropriate logger

--- src/main/java/be/fun/kata04/DataScannerLogger.java
+++ src/main/java/be/fun/kata04/DataScannerLogger.java
@@ -1,20 +1,35 @@
 package be.fun.kata04;
 
+import java.util.logging.Logger;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
-// Decorator - http://www.oodesign.com/decorator-pattern.html
+/**
+ *
+ *
+ * @param <T> the type of data that this {@code DataScanner} will produce
+ * @param <E> the type of {@code Exception} that this {@code DataScanner} may throw while parsing data
+ */
 public class DataScannerLogger<T, E extends Exception> implements DataScanner<T, E> {
+
+    private static final Logger LOGGER = Logger.getLogger(DataScannerLogger.class.getName());
+
+    // Decorator - http://www.oodesign.com/decorator-pattern.html
 
     private final DataScanner<T, E> scanner;
 
+    /**
+     *
+     *
+     * @param scanner
+     */
     public DataScannerLogger(final DataScanner<T, E> scanner) {
         this.scanner = checkNotNull(scanner);
     }
 
     public T scan() throws E {
         T data = scanner.scan();
-
-        System.out.println(data);
+        LOGGER.info(data.toString());
 
         return data;
     }
